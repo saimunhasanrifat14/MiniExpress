e08d78830a2c8050efb0904ecf86154367e449ac
Saimun Hasan <saimunhasanrifat14@gmail.com>
2025-07-05 16:16:19 +0600

    create a mini express

--- framework/app.js
+++ framework/app.js
@@ -1,20 +1,56 @@
+const { log } = require("console");
 const http = require("http");
 
 class App {
   constructor() {
     this.routes = [];
-    this.middlewares = [];
+    this.middleware = [];
+  }
+  // use method
+  use(middlewareFn) {
+    this.middleware.push(middlewareFn);
+  }
+  //get
+  get(path, handelerFn) {
+    this.routes.push({ method: "GET", path, handelerFn: handelerFn });
+  }
+  //post
+  post(path, handelerFn) {
+    this.routes.push({ method: "POST", path, handelerFn });
   }
 
-  //   use method to add middleware
-  use(middleware) {
-    this.middlewares.push(middleware);
+  executeMiddlware(req, res) {
+    const dispatch = (i = 0) => {
+      if (i >= this.middleware.length) return Promise.resolve();
+      const singleMw = this.middleware[i];
+      return new Promise((resolve, reject) => {
+        singleMw(req, res, () => {
+          resolve(dispatch(i + 1));
+        });
+      });
+    };
+    return dispatch(0);
   }
-  get(path, handler) {
-    this.routes.push({ path, method: "GET", handler });
-  }
-  post(path, handler) {
-    this.routes.push({ path, method: "POST", handler });
+
+  ourListen(port, callback) {
+    const server = http.createServer(async (req, res) => {
+      //middleware
+      await this.executeMiddlware(req, res);
+
+      //   search path
+      const searchRoutes = this.routes.find(
+        (r) => r.method == req.method && r.path == req.url
+      );
+      if (searchRoutes) {
+        searchRoutes.handelerFn(req, res);
+      } else {
+        res.writeHead(404, {
+          "content-type": "plain/text",
+        });
+        res.end("Routes Not Found");
+      }
+    });
+    server.listen(port, callback);
   }
 }
 
--- main.js
+++ main.js
@@ -1,16 +1,36 @@
-const miniexpress = require("./framework/app");
+/**
+ * project : miniExpress js ---> Routes , middleware , outSideRoutes
+ * tech js: promise ()
+ * tech nj : http  events
+ */
 
-const app = new miniexpress();
+const { log } = require("console");
+const miniExpress = require("./framework/app");
+const app = new miniExpress();
 
 app.use((req, res, next) => {
-  console.log("Middleware 1");
+  console.log("middleware funtion 1", req.url);
+  res.end("middleware 1");
   next();
 });
 
-app.get("/hello", (req, res) => {
-  res.end("Hello World");
+app.use((req, res, next) => {
+  console.log("middleware funtion 2", req.method);
+  next();
 });
 
-app.post("/data", (req, res) => {
-  res.end("Data received");
+app.get("/home", (req, res) => {
+  res.end("ami home route a ");
+});
+
+app.get("/about", (req, res) => {
+  log(req);
+});
+
+app.get("/mern", (req, res) => {
+  res.end("hello mern");
+});
+
+app.ourListen(3000, () => {
+  console.log(`Server Running on http://localhost:3000`);
 });
